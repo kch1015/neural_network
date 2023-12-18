c7637fbff3c15a9b1cf3c9bf5cc58320822abad2
PLT134340 <songwoo1015@gmail.com>
2023-12-18 16:51:51 +0900

    feat : Adam optimizer 생성

---
+++ project/numpy/optim/adam.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from .optimizer import _Optimizer
+
+
+class Adam(_Optimizer):
+    def __init__(self, params, lr, momentum=(0.9, 0.999)):
+        super().__init__(params, lr)
+
+        self.fig1, self.fig2 = momentum
+        self.m = [np.zeros_like(param) for param in self.params]
+        self.v = [np.zeros_like(param) for param in self.params]
+
+    def step(self, c=1e-15) -> None:
+        for i, param in enumerate(self.params):
+            self.m[i] = self.m[i] * self.fig1 + (1 - self.fig1) * param.grad
+            self.v[i] = self.v[i] * self.fig2 + (1 - self.fig2) * param.grad * param.grad
+            param.data -= self.lr * self.m[i] / (np.sqrt(self.v[i]) + c)
